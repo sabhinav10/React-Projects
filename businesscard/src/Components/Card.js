ef93c74ec5304dada3c3ffda9d2c3adb901362fc
sabhinav10 <sabhinav654@gmail.com>
2021-11-16 16:57:07 +0530

    Parent component

---
+++ businesscard/src/Components/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Info from "./Info";
+import "./styles.css"
+export default  function Card(){
+    return(
+        <div className="container">
+            <Info/>
+        </div>
+    )
+}
