bbb82fe49c8b2d108157312309a536f3e9530823
sabhinav10 <sabhinav654@gmail.com>
2021-11-16 16:56:51 +0530

    rendering

---
+++ businesscard/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Components/Card';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(<Card/>,document.getElementById("root"))
+
+
